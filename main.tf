2ea24f99d2244c4e8a803d9c46b0ad00d3470b83
Swarup <86089235+SwarupYonX@users.noreply.github.com>
2023-08-25 16:17:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-VNet"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/16"] # address space refers to the entire range of IP addresses available for allocation within a specific network.
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
@@ -41,7 +41,7 @@ resource "azurerm_subnet" "internal" {
   name                 = "${var.prefix}-internal"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.2.0/24"] # Address prefixes are subsets of the overall address space. They represent smaller, contiguous ranges of IP addresses carved out of the larger address space.
 }
 
 resource "azurerm_network_interface" "nic" {
